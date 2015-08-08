6d488096c0be5623c105c51a7505f992cffe8929
Michallis Pashidis <michallis@trust1team.com>
2015-08-08 13:58:46 +0200

    implement getApis implement getApi implement addApi

--- API-Engine-ejb/src/main/java/com/t1t/digipolis/rest/KongClient.java
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/rest/KongClient.java
@@ -1,8 +1,13 @@
 package com.t1t.digipolis.rest;
 
+import com.t1t.digipolis.kong.model.KongAPIList;
+import com.t1t.digipolis.kong.model.KongApi;
 import com.t1t.digipolis.kong.model.KongInfo;
 import com.t1t.digipolis.kong.model.KongStatus;
+import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
 
 /**
  * Created by michallispashidis on 7/08/15.
@@ -10,4 +15,7 @@ import retrofit.http.GET;
 public interface KongClient {
     @GET("/") KongInfo getInfo();
     @GET("/status") KongStatus getStatus();
+    @POST("/apis") KongApi addApi(@Body KongApi api);
+    @GET("/apis/{id}") KongApi getApi(@Path("id")String id);
+    @GET("/apis") KongAPIList getApiDefinitions();
 }
--- API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/KongPassthroughResource.java
+++ API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/KongPassthroughResource.java
@@ -1,5 +1,7 @@
 package com.t1t.digipolis.rest.resources;
 
+import com.t1t.digipolis.kong.model.KongAPIList;
+import com.t1t.digipolis.kong.model.KongApi;
 import com.t1t.digipolis.kong.model.KongInfo;
 import com.t1t.digipolis.kong.model.KongStatus;
 import com.t1t.digipolis.qualifier.APIEngineContext;
@@ -13,9 +15,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,14 +33,11 @@ public class KongPassthroughResource {
     @APIEngineContext
     private KongClient restClient;
 
-    @ApiOperation(value = "Test the connection",
-            notes = "Calling this endpoint will return a version number. It can be used to verify if a correct API-Engine URL was used.")
+    @ApiOperation(value = "Get Kong information",
+            notes = "Direct call to Kong for basic information")
     @ApiResponses({
-            @ApiResponse(code = 412, message = "JSON validation failed, a property is missing!"),
-            @ApiResponse(code = 415, message = "Unsupported mediatype, add HTTP header: 'Content-Type = application/json' to the request"),
-            @ApiResponse(code = 200, response = KongInfo.class, message = "Result of the request.")
+            @ApiResponse(code = 200, response = KongInfo.class, message = "Kong information.")
     })
-
     @GET
     @Produces("appilcation/json")
     public Response getInfo() {
@@ -48,11 +45,56 @@ public class KongPassthroughResource {
         return Response.status(200).entity(info).type(MediaType.APPLICATION_JSON).build();
     }
 
+    @ApiOperation(value = "Get Kong status",
+            notes = "Direct call to Kong for status information")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = KongStatus.class, message = "Kong status.")
+    })
     @GET
     @Path("/status")
     @Produces("application/json")
     public Response getStatus(){
         KongStatus stat = restClient.getStatus();
         return Response.status(200).entity(stat).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @ApiOperation(value = "Get API",
+            notes = "Get API based on the API id or name")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = KongApi.class, message = "API for the given id/name")
+    })
+    @GET
+    @Path("/apis/{id}")
+    @Produces("application/json")
+    public Response getApi(@PathParam("id")String id){
+        KongApi api = restClient.getApi(id);
+        return Response.status(200).entity(api).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @ApiOperation(value = "Add a new API",
+            notes = "Add a new API definition")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = KongApi.class, message = "Persisted API definition")
+    })
+    @POST
+    @Path("/apis")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addApi(KongApi api){
+        KongApi resultApi = restClient.addApi(api);
+        return Response.status(200).entity(resultApi).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @ApiOperation(value = "Get all APIs",
+            notes = "Get all defined APIs for the Kong gateway")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = KongAPIList.class, message = "List of API defintions")
+    })
+    @GET
+    @Path("/apis")
+    @Produces("application/json")
+    public Response getApi(){
+        KongAPIList apis = restClient.getApiDefinitions();
+        return Response.status(200).entity(apis).type(MediaType.APPLICATION_JSON).build();
     }
 }
