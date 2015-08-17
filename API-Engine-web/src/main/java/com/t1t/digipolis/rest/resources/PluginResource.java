96c5469cfc8e962c25fdd210ea04a7a536b34bb4
Michallis Pashidis <michallis@trust1tream.com>
2015-08-17 11:10:01 +0200

    implement pluginfacade
    Former-commit-id: 3a1aa8d4271d392739db163eec17afd59c50beb6

--- API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/PluginResource.java
+++ API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/PluginResource.java
@@ -1,24 +1,16 @@
 package com.t1t.digipolis.rest.resources;
 
-import com.t1t.digipolis.apim.beans.BeanUtils;
+import com.google.common.base.Preconditions;
 import com.t1t.digipolis.apim.beans.plugins.NewPluginBean;
 import com.t1t.digipolis.apim.beans.plugins.PluginBean;
-import com.t1t.digipolis.apim.beans.policies.PolicyDefinitionBean;
 import com.t1t.digipolis.apim.beans.summary.PluginSummaryBean;
 import com.t1t.digipolis.apim.beans.summary.PolicyDefinitionSummaryBean;
-import com.t1t.digipolis.apim.beans.summary.PolicyFormType;
-import com.t1t.digipolis.apim.common.plugin.Plugin;
-import com.t1t.digipolis.apim.common.plugin.PluginClassLoader;
-import com.t1t.digipolis.apim.common.plugin.PluginCoordinates;
 import com.t1t.digipolis.apim.core.IPluginRegistry;
 import com.t1t.digipolis.apim.core.IStorage;
 import com.t1t.digipolis.apim.core.IStorageQuery;
-import com.t1t.digipolis.apim.core.exceptions.InvalidPluginException;
-import com.t1t.digipolis.apim.core.exceptions.StorageException;
 import com.t1t.digipolis.apim.exceptions.*;
 import com.t1t.digipolis.apim.exceptions.NotAuthorizedException;
-import com.t1t.digipolis.apim.exceptions.i18n.Messages;
-import com.t1t.digipolis.apim.exceptions.ExceptionFactory;
+import com.t1t.digipolis.apim.facades.PluginFacade;
 import com.t1t.digipolis.apim.rest.resources.IPluginResource;
 import com.t1t.digipolis.apim.security.ISecurityContext;
 import com.t1t.digipolis.qualifier.APIEngineContext;
@@ -26,26 +18,18 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.apache.commons.io.IOUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.URL;
-import java.util.Date;
 import java.util.List;
 
 @Api(value = "/plugins", description = "The Plugin API.")
 @Path("/plugins")
 @ApplicationScoped
 public class PluginResource implements IPluginResource {
-
-    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Inject
     IStorage storage;
@@ -59,6 +43,8 @@ public class PluginResource implements IPluginResource {
     @Inject
     @APIEngineContext
     Logger log;
+    @Inject
+    private PluginFacade pluginFacade;
 
     /**
      * Constructor.
@@ -74,11 +60,7 @@ public class PluginResource implements IPluginResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<PluginSummaryBean> list() throws NotAuthorizedException {
-        try {
-            return query.listPlugins();
-        } catch (StorageException e) {
-            throw new SystemErrorException(e);
-        }
+        return pluginFacade.list();
     }
 
     @ApiOperation(value = "Add a Plugin",
@@ -92,66 +74,8 @@ public class PluginResource implements IPluginResource {
     public PluginBean create(NewPluginBean bean) throws PluginAlreadyExistsException, PluginNotFoundException {
         if (!securityContext.isAdmin())
             throw ExceptionFactory.notAuthorizedException();
-
-        PluginCoordinates coordinates = new PluginCoordinates(bean.getGroupId(), bean.getArtifactId(), bean.getVersion(),
-                bean.getClassifier(), bean.getType());
-        Plugin plugin = null;
-        try {
-            plugin = pluginRegistry.loadPlugin(coordinates);
-            bean.setName(plugin.getName());
-            bean.setDescription(plugin.getDescription());
-        } catch (InvalidPluginException e) {
-            throw new PluginNotFoundException(coordinates.toString(), e);
-        }
-
-        PluginBean pluginBean = new PluginBean();
-        pluginBean.setGroupId(bean.getGroupId());
-        pluginBean.setArtifactId(bean.getArtifactId());
-        pluginBean.setVersion(bean.getVersion());
-        pluginBean.setClassifier(bean.getClassifier());
-        pluginBean.setType(bean.getType());
-        pluginBean.setName(bean.getName());
-        pluginBean.setDescription(bean.getDescription());
-        pluginBean.setCreatedBy(securityContext.getCurrentUser());
-        pluginBean.setCreatedOn(new Date());
-        try {
-            if (storage.getPlugin(bean.getGroupId(), bean.getArtifactId()) != null) {
-                throw ExceptionFactory.pluginAlreadyExistsException();
-            }
-
-            storage.createPlugin(pluginBean);
-
-            // Process any contributed policy definitions.
-            List<URL> policyDefs = plugin.getPolicyDefinitions();
-            int policyDefCounter = 0;
-            for (URL url : policyDefs) {
-                PolicyDefinitionBean policyDef = (PolicyDefinitionBean) mapper.reader(PolicyDefinitionBean.class).readValue(url);
-                if (policyDef.getId() == null || policyDef.getId().trim().isEmpty()) {
-                    throw ExceptionFactory.policyDefInvalidException(Messages.i18n.format("PluginResourceImpl.MissingPolicyDefId", policyDef.getName())); //$NON-NLS-1$
-                }
-                policyDef.setPluginId(pluginBean.getId());
-                if (policyDef.getId() == null) {
-                    policyDef.setId(BeanUtils.idFromName(policyDef.getName()));
-                } else {
-                    policyDef.setId(BeanUtils.idFromName(policyDef.getId()));
-                }
-                if (policyDef.getFormType() == null) {
-                    policyDef.setFormType(PolicyFormType.Default);
-                }
-                if (storage.getPolicyDefinition(policyDef.getId()) == null) {
-                    storage.createPolicyDefinition(policyDef);
-                    policyDefCounter++;
-                }
-            }
-            log.info(String.format("Created plugin mvn:%s:%s:%s", pluginBean.getGroupId(), pluginBean.getArtifactId(),  //$NON-NLS-1$
-                    pluginBean.getVersion()));
-            log.info(String.format("\tCreated %s policy definitions from plugin.", String.valueOf(policyDefCounter))); //$NON-NLS-1$
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
-        return pluginBean;
+        Preconditions.checkNotNull(bean);
+        return pluginFacade.create(bean);
     }
 
     @ApiOperation(value = "Get Plugin by ID",
@@ -163,19 +87,9 @@ public class PluginResource implements IPluginResource {
     @Path("/{pluginId}")
     @Produces(MediaType.APPLICATION_JSON)
     public PluginBean get(@PathParam("pluginId") Long pluginId) throws PluginNotFoundException, NotAuthorizedException {
-        if (!securityContext.isAdmin())
-            throw ExceptionFactory.notAuthorizedException();
-        try {
-            PluginBean bean = storage.getPlugin(pluginId);
-            if (bean == null) {
-                throw ExceptionFactory.pluginNotFoundException(pluginId);
-            }
-            return bean;
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        if (!securityContext.isAdmin()) throw ExceptionFactory.notAuthorizedException();
+        Preconditions.checkNotNull(pluginId);
+        return pluginFacade.get(pluginId);
     }
 
     @ApiOperation(value = "Delete a Plugin by ID",
@@ -187,21 +101,9 @@ public class PluginResource implements IPluginResource {
     @Path("/{pluginId}")
     public void delete(@PathParam("pluginId") Long pluginId) throws PluginNotFoundException,
             NotAuthorizedException {
-        if (!securityContext.isAdmin())
-            throw ExceptionFactory.notAuthorizedException();
-        try {
-            PluginBean pbean = storage.getPlugin(pluginId);
-            if (pbean == null) {
-                throw ExceptionFactory.pluginNotFoundException(pluginId);
-            }
-            storage.deletePlugin(pbean);
-            log.info(String.format("Deleted plugin mvn:%s:%s:%s", pbean.getGroupId(), pbean.getArtifactId(),  //$NON-NLS-1$
-                    pbean.getVersion()));
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        if (!securityContext.isAdmin()) throw ExceptionFactory.notAuthorizedException();
+        Preconditions.checkNotNull(pluginId);
+        pluginFacade.delete(pluginId);
     }
 
     @ApiOperation(value = "Get Plugin Policy Definitions",
@@ -213,12 +115,8 @@ public class PluginResource implements IPluginResource {
     @Path("/{pluginId}/policyDefs")
     @Produces(MediaType.APPLICATION_JSON)
     public List<PolicyDefinitionSummaryBean> getPolicyDefs(@PathParam("pluginId") Long pluginId) throws PluginNotFoundException {
-        get(pluginId);
-        try {
-            return query.listPluginPolicyDefs(pluginId);
-        } catch (StorageException e) {
-            throw new SystemErrorException(e);
-        }
+        Preconditions.checkNotNull(pluginId);
+        return pluginFacade.getPolicyDefs(pluginId);
     }
 
     @ApiOperation(value = "Get Plugin Policy Form",
@@ -231,85 +129,6 @@ public class PluginResource implements IPluginResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getPolicyForm(@PathParam("pluginId") Long pluginId, @PathParam("policyDefId") String policyDefId) throws PluginNotFoundException,
             PluginResourceNotFoundException, PolicyDefinitionNotFoundException {
-        PluginBean pbean = null;
-        PolicyDefinitionBean pdBean = null;
-        try {
-            pbean = storage.getPlugin(pluginId);
-            if (pbean == null) {
-                throw ExceptionFactory.pluginNotFoundException(pluginId);
-            }
-            pdBean = storage.getPolicyDefinition(policyDefId);
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
-        PluginCoordinates coordinates = new PluginCoordinates(pbean.getGroupId(), pbean.getArtifactId(),
-                pbean.getVersion(), pbean.getClassifier(), pbean.getType());
-        try {
-            if (pdBean == null) {
-                throw ExceptionFactory.policyDefNotFoundException(policyDefId);
-            }
-            if (pdBean.getPluginId() == null || !pdBean.getPluginId().equals(pbean.getId())) {
-                throw ExceptionFactory.pluginNotFoundException(pluginId);
-            }
-            if (pdBean.getFormType() == PolicyFormType.JsonSchema && pdBean.getForm() != null) {
-                String formPath = pdBean.getForm();
-                if (!formPath.startsWith("/")) { //$NON-NLS-1$
-                    formPath = "META-INF/apiman/policyDefs/" + formPath; //$NON-NLS-1$
-                } else {
-                    formPath = formPath.substring(1);
-                }
-                Plugin plugin = pluginRegistry.loadPlugin(coordinates);
-                PluginClassLoader loader = plugin.getLoader();
-                InputStream resource = null;
-                try {
-                    resource = loader.getResourceAsStream(formPath);
-                    if (resource == null) {
-                        throw ExceptionFactory.pluginResourceNotFoundException(formPath, coordinates);
-                    }
-                    StringWriter writer = new StringWriter();
-                    IOUtils.copy(resource, writer);
-                    return writer.toString();
-                } finally {
-                    IOUtils.closeQuietly(resource);
-                }
-            } else {
-                throw ExceptionFactory.pluginResourceNotFoundException(null, coordinates);
-            }
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Throwable t) {
-            throw new SystemErrorException(t);
-        }
+        return pluginFacade.getPolicyForm(pluginId, policyDefId);
     }
-
-    /**
-     * @return the storage
-     */
-    public IStorage getStorage() {
-        return storage;
-    }
-
-    /**
-     * @param storage the storage to set
-     */
-    public void setStorage(IStorage storage) {
-        this.storage = storage;
-    }
-
-    /**
-     * @return the securityContext
-     */
-    public ISecurityContext getSecurityContext() {
-        return securityContext;
-    }
-
-    /**
-     * @param securityContext the securityContext to set
-     */
-    public void setSecurityContext(ISecurityContext securityContext) {
-        this.securityContext = securityContext;
-    }
-
 }
