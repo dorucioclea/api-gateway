64de431cfa341d3215bc195fd716e48a23e6fe98
Michallis Pashidis <michallis@trust1tream.com>
2015-08-17 10:22:03 +0200

    implement policydef facade
    Former-commit-id: 22cf9885562bf9fe66751dca479fc72b7cdf36f8

--- API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/PolicyDefinitionResource.java
+++ API-Engine-web/src/main/java/com/t1t/digipolis/rest/resources/PolicyDefinitionResource.java
@@ -1,17 +1,16 @@
 package com.t1t.digipolis.rest.resources;
 
-import com.t1t.digipolis.apim.beans.BeanUtils;
+import com.google.common.base.Preconditions;
 import com.t1t.digipolis.apim.beans.policies.PolicyDefinitionBean;
 import com.t1t.digipolis.apim.beans.policies.UpdatePolicyDefinitionBean;
 import com.t1t.digipolis.apim.beans.summary.PolicyDefinitionSummaryBean;
-import com.t1t.digipolis.apim.beans.summary.PolicyFormType;
 import com.t1t.digipolis.apim.core.IStorage;
 import com.t1t.digipolis.apim.core.IStorageQuery;
-import com.t1t.digipolis.apim.core.exceptions.StorageException;
-import com.t1t.digipolis.apim.exceptions.*;
-import com.t1t.digipolis.apim.exceptions.NotAuthorizedException;
-import com.t1t.digipolis.apim.exceptions.i18n.Messages;
 import com.t1t.digipolis.apim.exceptions.ExceptionFactory;
+import com.t1t.digipolis.apim.exceptions.NotAuthorizedException;
+import com.t1t.digipolis.apim.exceptions.PolicyDefinitionAlreadyExistsException;
+import com.t1t.digipolis.apim.exceptions.PolicyDefinitionNotFoundException;
+import com.t1t.digipolis.apim.facades.PolicyDefinitionFacade;
 import com.t1t.digipolis.apim.rest.resources.IPolicyDefinitionResource;
 import com.t1t.digipolis.apim.security.ISecurityContext;
 import com.t1t.digipolis.qualifier.APIEngineContext;
@@ -19,6 +18,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -38,8 +38,12 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     IStorageQuery query;
     @Inject
     ISecurityContext securityContext;
-    @Inject @APIEngineContext
+    @Inject
+    @APIEngineContext
     Logger log;
+    @Inject
+    private PolicyDefinitionFacade policyDefinitionFacade;
+
     /**
      * Constructor.
      */
@@ -54,11 +58,7 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<PolicyDefinitionSummaryBean> list() throws NotAuthorizedException {
-        try {
-            return query.listPolicyDefinitions();
-        } catch (StorageException e) {
-            throw new SystemErrorException(e);
-        }
+        return policyDefinitionFacade.list();
     }
 
     @ApiOperation(value = "Add Policy Definition",
@@ -70,29 +70,9 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public PolicyDefinitionBean create(PolicyDefinitionBean bean) throws PolicyDefinitionAlreadyExistsException {
-        if (!securityContext.isAdmin())
-            throw ExceptionFactory.notAuthorizedException();
-        // Auto-generate an ID if one isn't provided.
-        if (bean.getId() == null || bean.getId().trim().isEmpty()) {
-            bean.setId(BeanUtils.idFromName(bean.getName()));
-        } else {
-            bean.setId(BeanUtils.idFromName(bean.getId()));
-        }
-        try {
-            if (storage.getPolicyDefinition(bean.getId()) != null) {
-                throw ExceptionFactory.policyDefAlreadyExistsException(bean.getName());
-            }
-            if (bean.getFormType() == null) {
-                bean.setFormType(PolicyFormType.Default);
-            }
-            // Store/persist the new policyDef
-            storage.createPolicyDefinition(bean);
-            return bean;
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        if (!securityContext.isAdmin()) throw ExceptionFactory.notAuthorizedException();
+        Preconditions.checkNotNull(bean);
+        return policyDefinitionFacade.create(bean);
     }
 
     @ApiOperation(value = "Get Policy Definition by ID",
@@ -104,17 +84,8 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     @Path("/{policyDefinitionId}")
     @Produces(MediaType.APPLICATION_JSON)
     public PolicyDefinitionBean get(@PathParam("policyDefinitionId") String policyDefinitionId) throws PolicyDefinitionNotFoundException, NotAuthorizedException {
-        try {
-            PolicyDefinitionBean bean = storage.getPolicyDefinition(policyDefinitionId);
-            if (bean == null) {
-                throw ExceptionFactory.policyDefNotFoundException(policyDefinitionId);
-            }
-            return bean;
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        Preconditions.checkArgument(!StringUtils.isEmpty(policyDefinitionId));
+        return policyDefinitionFacade.get(policyDefinitionId);
     }
 
     @ApiOperation(value = "Update Policy Definition",
@@ -126,28 +97,10 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     @Path("/{policyDefinitionId}")
     public void update(@PathParam("policyDefinitionId") String policyDefinitionId, UpdatePolicyDefinitionBean bean)
             throws PolicyDefinitionNotFoundException, NotAuthorizedException {
-        if (!securityContext.isAdmin())
-            throw ExceptionFactory.notAuthorizedException();
-        try {
-            PolicyDefinitionBean pdb = storage.getPolicyDefinition(policyDefinitionId);
-            if (pdb == null) {
-                throw ExceptionFactory.policyDefNotFoundException(policyDefinitionId);
-            }
-            if (pdb.getPluginId() != null) {
-                throw new SystemErrorException(Messages.i18n.format("CannotUpdatePluginPolicyDef")); //$NON-NLS-1$
-            }
-            if (bean.getName() != null)
-                pdb.setName(bean.getName());
-            if (bean.getDescription() != null)
-                pdb.setDescription(bean.getDescription());
-            if (bean.getIcon() != null)
-                pdb.setIcon(bean.getIcon());
-            storage.updatePolicyDefinition(pdb);
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        if (!securityContext.isAdmin()) throw ExceptionFactory.notAuthorizedException();
+        Preconditions.checkArgument(!StringUtils.isEmpty(policyDefinitionId));
+        Preconditions.checkNotNull(bean);
+        policyDefinitionFacade.update(policyDefinitionId, bean);
     }
 
     @ApiOperation(value = "Delete policy definition.",
@@ -159,50 +112,8 @@ public class PolicyDefinitionResource implements IPolicyDefinitionResource {
     @Path("/{policyDefinitionId}")
     public void delete(@PathParam("policyDefinitionId") String policyDefinitionId) throws PolicyDefinitionNotFoundException,
             NotAuthorizedException {
-        if (!securityContext.isAdmin())
-            throw ExceptionFactory.notAuthorizedException();
-        try {
-            PolicyDefinitionBean pdb = storage.getPolicyDefinition(policyDefinitionId);
-            if (pdb == null) {
-                throw ExceptionFactory.policyDefNotFoundException(policyDefinitionId);
-            }
-            if (pdb.getPluginId() != null) {
-                throw new SystemErrorException(Messages.i18n.format("CannotDeletePluginPolicyDef")); //$NON-NLS-1$
-            }
-            storage.deletePolicyDefinition(pdb);
-        } catch (AbstractRestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new SystemErrorException(e);
-        }
+        if (!securityContext.isAdmin()) throw ExceptionFactory.notAuthorizedException();
+        Preconditions.checkArgument(!StringUtils.isEmpty(policyDefinitionId));
+        policyDefinitionFacade.delete(policyDefinitionId);
     }
-
-    /**
-     * @return the storage
-     */
-    public IStorage getStorage() {
-        return storage;
-    }
-
-    /**
-     * @param storage the storage to set
-     */
-    public void setStorage(IStorage storage) {
-        this.storage = storage;
-    }
-
-    /**
-     * @return the securityContext
-     */
-    public ISecurityContext getSecurityContext() {
-        return securityContext;
-    }
-
-    /**
-     * @param securityContext the securityContext to set
-     */
-    public void setSecurityContext(ISecurityContext securityContext) {
-        this.securityContext = securityContext;
-    }
-
 }
