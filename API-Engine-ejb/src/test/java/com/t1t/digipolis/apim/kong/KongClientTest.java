2463b48b426d7b44268619dff4a72915a32b7c3e
Michallis Pashidis <michallis@trust1tream.com>
2015-08-18 21:23:28 +0200

    provide test stub for kong client
    Former-commit-id: d103f5d192e017fd6e2dba74ff6115aef6f2b5a5

---
+++ API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/kong/KongClientTest.java
@@ -0,0 +1,133 @@
+package com.t1t.digipolis.apim.kong;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by michallispashidis on 18/08/15.
+ */
+public class KongClientTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInfo() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStatus() throws Exception {
+
+    }
+
+    @Test
+    public void testAddApi() throws Exception {
+
+    }
+
+    @Test
+    public void testGetApi() throws Exception {
+
+    }
+
+    @Test
+    public void testListApis() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateApi() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateOrCreateApi() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteApi() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateConsumer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConsumer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConsumers() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateConsumer() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateOrCreateConsumer() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteConsumer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEnabledPlugins() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPluginSchema() throws Exception {
+
+    }
+
+    @Test
+    public void testCreatePlugin() throws Exception {
+
+    }
+
+    @Test
+    public void testGetApiPluginList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllPlugins() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePlugin() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateOrUpdatePlugin() throws Exception {
+
+    }
+
+    @Test
+    public void testDeletePlugin() throws Exception {
+
+    }
+}
