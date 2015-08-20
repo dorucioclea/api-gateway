10ed9194355bc2cd686f230bdb00be1707960ddc
Michallis Pashidis <michallis@trust1tream.com>
2015-08-20 18:52:09 +0200

    update mysql script to use Kong default gateway
    Former-commit-id: c7359df1d196cd26079c27507f8ae186c93c4253

---
+++ API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/kong/KongUtils.java
@@ -0,0 +1,42 @@
+package com.t1t.digipolis.apim.kong;
+
+import com.t1t.digipolis.apim.beans.gateways.RestGatewayConfigBean;
+import com.t1t.digipolis.apim.common.util.AesEncrypter;
+import org.apache.commons.codec.binary.Base64;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by michallispashidis on 20/08/15.
+ */
+public class KongUtils {
+    private static Logger log = LoggerFactory.getLogger(KongUtils.class.getName());
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Utility method that encodes a name and password for basic authentication.
+     * @param name
+     * @param password
+     * @return
+     */
+    public static String getBasicAuthHeaderValueEncoded(String name, String password){
+        String authString = name + ":" + password;
+        System.out.println("auth string: " + authString);
+        byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+        return new String(authEncBytes);
+    }
+
+    /**
+     * Utility to encrypt
+     * @param configuration
+     * @return
+     */
+    public String encryptGatewayConfig(RestGatewayConfigBean configBean){
+        return AesEncrypter.encrypt(configuration);
+    }
+
+    public String decryptGatewayConfig(String configuration){
+        return AesEncrypter.decrypt(configuration);
+    }
+}
