634a51068421a5f9a6e19a9e9b5a3b3b30d958a3
Michallis Pashidis <michallis@trust1tream.com>
2015-08-27 18:24:47 +0200

    add info to test case
    Former-commit-id: 94e6cbd9f5951776797e40f864b481c886f3d638

--- API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/kong/KongBasicAuthTest.java
+++ API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/kong/KongBasicAuthTest.java
@@ -1,6 +1,7 @@
 package com.t1t.digipolis.apim.kong;
 
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.gson.Gson;
 import com.t1t.digipolis.apim.beans.gateways.RestGatewayConfigBean;
 import com.t1t.digipolis.kong.model.KongInfo;
@@ -11,12 +12,16 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import retrofit.RetrofitError;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -43,13 +48,23 @@ public class KongBasicAuthTest {
     private static final String KONG_UNDER_TEST_URL = "http://apim.t1t.be:8001";//should point to the admin url:port
     //private static final String KONG_UNDER_TEST_URL = "http://localhost:8001";//should point to the admin url:port
     private static final String API_NAME = "newapibasicauth";
+    private static final int JETTY_SERVER_HTTP_PORT = 8089;
+    private static final int JETTY_SERVER_HTTPS_PORT = 8489;
     private static final String API_PATH = "/testbasicauthpath";
     private static final String API_URL = "http://domain.com/app/rest/v1";
 
+/*  If you want to use the same server instance for all tests:
     @ClassRule
-    public static WireMockClassRule wireMockRule = new WireMockClassRule(wireMockConfig().port(8089).httpsPort(8489));
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(wireMockConfig()
+            .port(JETTY_SERVER_HTTP_PORT)
+            .httpsPort(JETTY_SERVER_HTTPS_PORT));
     @Rule
-    public WireMockClassRule instanceRule = wireMockRule;
+    public WireMockClassRule instanceRule = wireMockRule;*/
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig()
+            .port(JETTY_SERVER_HTTP_PORT)
+            .httpsPort(JETTY_SERVER_HTTPS_PORT));
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -74,6 +89,10 @@ public class KongBasicAuthTest {
         }
     }
 
+    /**
+     * Test if the Kong server is reachable, by requesting basic information.
+     * @throws Exception
+     */
     @Test
     public void testGetInfo() throws Exception {
         KongInfo kongInfo = kongClient.getInfo();
@@ -90,28 +109,95 @@ public class KongBasicAuthTest {
         assertTrue(plugins.getAvailableOnServer().size() > 0);
     }
 
+    /**
+     * Test that the jetty server basic test runs fine.
+     */
     @Test
     public void exampleTest() {
-        stubFor(get(urlEqualTo("/some/thing"))
-                .withHeader("Accept", equalTo("text/xml"))
+        stubFor(get(urlEqualTo(API_PATH))
+                .withHeader("Accept", equalTo("application/json"))
                 .willReturn(aResponse()
                         .withStatus(200)
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<response>Some content</response>")));
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"version\" : \"1.0\"}")));
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("http://localhost:8089/some/thing");
-        request.setHeader(new BasicHeader("Accept", "text/xml"));
+        HttpGet request = new HttpGet("http://localhost:"+JETTY_SERVER_HTTP_PORT+API_PATH);
+        request.setHeader(new BasicHeader("Accept", "application/json"));
         try {
             HttpResponse response = client.execute(request);
             log.info("Response:{}", response);
+            String resultJson = parseJSONBody(response.getEntity().getContent());
+            log.info("Response body:{}", resultJson);
             assertTrue(response.getStatusLine().getStatusCode()==200);
+            TestVersion testVersion = gson.fromJson(resultJson, TestVersion.class);
+            assertTrue(testVersion.getVersion().equals("1.0"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    @Test
+    public void basicAuthTest(){
+        //setup endpoint
+        stubFor(get(urlEqualTo(API_PATH))
+                .withHeader("Accept", equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"version\" : \"1.0\"}")));
+        //register api
+        //test api without basic auth
+        //enable basic auth on api
+        //create consumer
+        //create login/pwd for consumer
+        //access resource with encode Basic Auth header
+    }
+
+    /**
+     * Print json utility for Kong objects
+     * @param obj
+     */
     private void print(Object obj) {
         log.info(gson.toJson(obj));
+    }
+
+    /**
+     * Utility method to parse apache response body inputstream.
+     * @param is
+     * @return
+     */
+    private String parseJSONBody(InputStream is){
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        try {
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
+
+/**
+ * Utility Test version object to serialize/deserialize dummy server response.
+ */
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+class TestVersion{
+    private String version;
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "TestVersion{" +
+                "version='" + version + '\'' +
+                '}';
     }
 }
