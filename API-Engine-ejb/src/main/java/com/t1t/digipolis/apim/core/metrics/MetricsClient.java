bb9074e0a4f15b5752c0eb42761dabd802322582
Michallis Pashidis <michallis@trust1tream.com>
2015-09-22 14:47:24 +0200

    DPAPIM-109 add interval DPAPIM-109 add service definition
    Former-commit-id: e4a397aec90f78ab27236c9924ed7811e667c2fa

---
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/core/metrics/MetricsClient.java
@@ -0,0 +1,50 @@
+package com.t1t.digipolis.apim.core.metrics;
+
+import com.google.gson.JsonObject;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by michallispashidis on 7/08/15.
+ */
+public interface MetricsClient {
+    /*********************
+     * GENERAL
+     *******************/
+    @GET("/usage/{organizationId}/{serviceId}/{version}/{interval}/{from}/{to}")
+    JsonObject getServiceUsageFromToInterval(@Path("organizationId") String orgId,
+                                             @Path("serviceId") String serviceId,
+                                             @Path("version") String version,
+                                             @Path("interval") TimeInterval interval,
+                                             @Path("from") String from,
+                                             @Path("to") String to
+    );
+
+    @GET("/response-stats/{organizationId}/{serviceId}/{version}/{interval}/{from}/{to}")
+    JsonObject getServiceResponseStatisticsFromToInterval(@Path("organizationId") String orgId,
+                                                          @Path("serviceId") String serviceId,
+                                                          @Path("version") String version,
+                                                          @Path("interval") TimeInterval interval,
+                                                          @Path("from") String from,
+                                                          @Path("to") String to
+    );
+
+    @GET("/response-summary/{organizationId}/{serviceId}/{version}/{from}/{to}")
+    JsonObject getServiceResponseSummaryFromTo(@Path("organizationId") String orgId,
+                                               @Path("serviceId") String serviceId,
+                                               @Path("version") String version,
+                                               @Path("from") String from,
+                                               @Path("to") String to
+    );
+
+    @GET("/consumer-usage/{organizationId}/{serviceId}/{version}/{interval}/{from}/{to}/{consumer}")
+    JsonObject getServiceConsumerUsageFromToInterval(@Path("organizationId") String orgId,
+                                                          @Path("serviceId") String serviceId,
+                                                          @Path("version") String version,
+                                                          @Path("interval") TimeInterval interval,
+                                                          @Path("from") String from,
+                                                          @Path("to") String to,
+                                                          @Path("consumer") String consumerId
+    );
+
+}
---
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/core/metrics/TimeInterval.java
@@ -0,0 +1,8 @@
+package com.t1t.digipolis.apim.core.metrics;
+
+/**
+ * Created by michallispashidis on 22/09/15.
+ */
+public enum TimeInterval {
+    minute, hour, day, week, month
+}
