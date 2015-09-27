610c341a846500aec17eb01d19d7d7e43ed1bb7b
Michallis Pashidis <michallis@trust1tream.com>
2015-09-27 13:39:27 +0200

    DPAPIM-106 add redirect impl for authorization using consent page
    Former-commit-id: 588e30f7e6e745a23f8e3860865bcce5e4a20651

--- Api-Engine-auth/src/main/java/com/t1t/digipolis/apim/auth/rest/resources/IOAuth2Authorization.java
+++ Api-Engine-auth/src/main/java/com/t1t/digipolis/apim/auth/rest/resources/IOAuth2Authorization.java
@@ -2,10 +2,13 @@ package com.t1t.digipolis.apim.auth.rest.resources;
 
 import com.t1t.digipolis.apim.beans.authorization.OAuthApplicationResponse;
 import com.t1t.digipolis.apim.beans.authorization.OAuthConsumerRequestBean;
+import com.t1t.digipolis.apim.beans.authorization.OAuthResponseType;
 import com.t1t.digipolis.apim.exceptions.OAuthException;
 import com.t1t.digipolis.kong.model.KongPluginOAuthConsumerRequest;
 import com.t1t.digipolis.kong.model.KongPluginOAuthConsumerResponse;
 import com.t1t.digipolis.kong.model.KongPluginOAuthConsumerResponseList;
+
+import javax.ws.rs.PathParam;
 
 /**
  * Created by michallispashidis on 16/09/15.
@@ -27,5 +30,18 @@ public interface IOAuth2Authorization {
      * @throws OAuthException
      */
     OAuthApplicationResponse getApplicationInfo(String oauthClientId, String orgId, String serviceId, String version) throws OAuthException;
+
+    /**
+     * Get redirect URL for user authorization using Authorization Grant/Implicit Grant.
+     *
+     * @param oauthClientId
+     * @param orgId
+     * @param serviceId
+     * @param version
+     * @param responseType
+     * @return
+     * @throws OAuthException
+     */
+    public String getAuthorizationRedirect(OAuthResponseType responseType, String oauthClientId,String orgId,String serviceId,String version) throws OAuthException;
 
 }
