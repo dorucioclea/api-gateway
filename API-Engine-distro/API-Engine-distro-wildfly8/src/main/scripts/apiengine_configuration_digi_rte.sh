ab578028690ef62477f1a90aceb7285bc1658938
Michallis Pashidis <michallis@trust1team.com>
2015-10-16 17:44:01 +0200

    update move to ACC
    Former-commit-id: 0455b09f0e849dc369a8479084c9de7590d715f1

--- API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_digi_rte.sh
+++ API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_digi_rte.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #register api engine
 curl -i -X POST \
-  --url http://rasu076.rte.digant.antwerpen.local:8001/apis/ \
+  --url http://rasu076.rte.antwerpen.local:8001/apis/ \
   --data 'path=/rte/apiengine/v1' \
   --data 'name=rte.apiengine.v1' \
   --data 'target_url=http://rasu073.rte.antwerpen.local/API-Engine-web/v1/' \
   --data 'strip_path=true'
 
 #enable CORS
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengine.v1/plugins \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengine.v1/plugins \
     --data "name=cors" \
     --data "value.origin=*" \
     --data "value.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
@@ -17,28 +17,28 @@ curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengine.v
     --data "value.max_age=3600"
 
 #enable Keyauth
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengine.v1/plugins \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengine.v1/plugins \
     --data "name=keyauth" \
     --data "value.key_names=apikey"
 
 #create marketplace consumer, username should be unique
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/consumers \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/consumers \
     --data "username=rte.marketplace.v1"
 
 #create publisher consumer
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/consumers \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/consumers \
     --data "username=rte.publisher.v1"
 
 #enable keyauth for marketplace and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/consumers/rte.marketplace.v1/keyauth \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/consumers/rte.marketplace.v1/keyauth \
     --data "key=229e2ea08ba94919c9d221cdf3be1f7d"
 
 #enable keyauth for publisher and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/consumers/rte.publisher.v1/keyauth \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/consumers/rte.publisher.v1/keyauth \
     --data "key=***REMOVED***"
 
 #Analytics
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengine.v1/plugins/ \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengine.v1/plugins/ \
     --data "name=mashape-analytics" \
     --data "value.service_token=558a95f80f7a734609de5c04" \
     --data "value.environment=RTE"
@@ -48,14 +48,14 @@ curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengine.v
 
 #register api engine authorization endpoints
 curl -i -X POST \
-  --url http://rasu076.rte.digant.antwerpen.local:8001/apis/ \
+  --url http://rasu076.rte.antwerpen.local:8001/apis/ \
   --data 'path=/rte/apiengineauth/v1' \
   --data 'name=rte.apiengineauth.v1' \
   --data 'target_url=http://rasu073.rte.antwerpen.local/API-Engine-auth/v1/' \
   --data 'strip_path=true'
 
 #enable CORS
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins \
     --data "name=cors" \
     --data "value.origin=*" \
     --data "value.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
@@ -64,12 +64,12 @@ curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengineau
     --data "value.max_age=3600"
 
 #enable Keyauth
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins \
     --data "name=keyauth" \
     --data "value.key_names=apikey"
 
 #Analytics
-curl -X POST http://rasu076.rte.digant.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins/ \
+curl -X POST http://rasu076.rte.antwerpen.local:8001/apis/rte.apiengineauth.v1/plugins/ \
     --data "name=mashape-analytics" \
     --data "value.service_token=558a95f80f7a734609de5c04" \
     --data "value.environment=RTE"
