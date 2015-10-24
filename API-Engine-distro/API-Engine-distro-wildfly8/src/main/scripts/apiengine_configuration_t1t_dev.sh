85fedee543242c759156a8bd8dd2ab3d5c7be4d3
Michallis Pashidis <michallis@trust1tream.com>
2015-10-24 17:29:37 +0200

    update for kong 0.5.2
    Former-commit-id: 999378bb318dcc497956b9771c32167dd1d51e39

--- API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_t1t_dev.sh
+++ API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_t1t_dev.sh
@@ -2,24 +2,24 @@
 #register api engine
 curl -i -X POST \
   --url http://apim.t1t.be:8001/apis/ \
-  --data 'path=/dev/apiengine/v1' \
+  --data 'request_path=/dev/apiengine/v1' \
   --data 'name=dev.apiengine.v1' \
-  --data 'target_url=http://api.t1t.be/API-Engine-web/v1/' \
-  --data 'strip_path=true'
+  --data 'upstream_url=http://api.t1t.be/API-Engine-web/v1/' \
+  --data 'strip_request_path=true'
 
 #enable CORS
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
     --data "name=cors" \
-    --data "value.origin=*" \
-    --data "value.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
-    --data "value.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, apikey, Authorization" \
-    --data "value.credentials=true" \
-    --data "value.max_age=3600"
+    --data "config.origin=*" \
+    --data "config.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
+    --data "config.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, apikey, Authorization" \
+    --data "config.credentials=true" \
+    --data "config.max_age=3600"
 
 #enable Keyauth
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
-    --data "name=keyauth" \
-    --data "value.key_names=apikey"
+    --data "name=key-auth" \
+    --data "config.key_names=apikey"
 
 #create marketplace consumer, username should be unique
 curl -X POST http://apim.t1t.be:8001/consumers \
@@ -30,17 +30,17 @@ curl -X POST http://apim.t1t.be:8001/consumers \
     --data "username=dev.publisher.v1"
 
 #enable keyauth for marketplace and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://apim.t1t.be:8001/consumers/dev.marketplace.v1/keyauth \
+curl -X POST http://apim.t1t.be:8001/consumers/dev.marketplace.v1/key-auth \
     --data "key=6b8406cc81fe4ca3cc9cd4a0abfb97c2"
 
 #enable keyauth for publisher and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://apim.t1t.be:8001/consumers/dev.publisher.v1/keyauth \
+curl -X POST http://apim.t1t.be:8001/consumers/dev.publisher.v1/key-auth \
     --data "key=***REMOVED***"
 
 #Analytics
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins/ \
     --data "name=mashape-analytics" \
-    --data "value.service_token=558a95f80f7a734609de5c04"
+    --data "config.service_token=558a95f80f7a734609de5c04"
 
 
 
@@ -48,56 +48,26 @@ curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins/ \
 #register api engine authorization endpoints
 curl -i -X POST \
   --url http://apim.t1t.be:8001/apis/ \
-  --data 'path=/dev/apiengineauth/v1' \
+  --data 'request_path=/dev/apiengineauth/v1' \
   --data 'name=dev.apiengineauth.v1' \
-  --data 'target_url=http://api.t1t.be/API-Engine-auth/v1/' \
-  --data 'strip_path=true'
+  --data 'upstream_url=http://api.t1t.be/API-Engine-auth/v1/' \
+  --data 'strip_request_path=true'
 
 #enable CORS
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
     --data "name=cors" \
-    --data "value.origin=*" \
-    --data "value.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
-    --data "value.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, apikey, Authorization" \
-    --data "value.credentials=true" \
-    --data "value.max_age=3600"
+    --data "config.origin=*" \
+    --data "config.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
+    --data "config.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, apikey, Authorization" \
+    --data "config.credentials=true" \
+    --data "config.max_age=3600"
 
 #enable Keyauth
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
-    --data "name=keyauth" \
-    --data "value.key_names=apikey"
+    --data "name=key-auth" \
+    --data "config.key_names=apikey"
 
 #Analytics
 curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins/ \
     --data "name=mashape-analytics" \
-    --data "value.service_token=558a95f80f7a734609de5c04"
-
-
-
-
-#register api engine authorization CONSENT PAGE
-curl -i -X POST \
-  --url http://apim.t1t.be:8001/apis/ \
-  --data 'path=/dev/apiengineauth/consent' \
-  --data 'name=dev.apiengineauth.consent' \
-  --data 'target_url=http://api.t1t.be/API-Engine-auth/faces/' \
-  --data 'strip_path=true'
-
-#enable CORS
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.consent/plugins \
-    --data "name=cors" \
-    --data "value.origin=*" \
-    --data "value.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
-    --data "value.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, apikey, Authorization" \
-    --data "value.credentials=true" \
-    --data "value.max_age=3600"
-
-#enable Keyauth
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.consent/plugins \
-    --data "name=keyauth" \
-    --data "value.key_names=apikey"
-
-#Analytics
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.consent/plugins/ \
-    --data "name=mashape-analytics" \
-    --data "value.service_token=558a95f80f7a734609de5c04"
+    --data "config.service_token=558a95f80f7a734609de5c04"
