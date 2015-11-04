8eb5aea394c01d0fdb8eb5fd46983aac8e721a9d
Michallis Pashidis <michallis@trust1team.com>
2015-11-04 13:32:44 +0100

    update url due to dns change
    Former-commit-id: 908e693800377e192fe99c1e2b0ec7a75cdb7311

--- API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_t1t_dev.sh
+++ API-Engine-distro/API-Engine-distro-wildfly8/src/main/scripts/apiengine_configuration_t1t_dev.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #register api engine
 curl -i -X POST \
-  --url http://apim.t1t.be:8001/apis/ \
+  --url http://dev.apim.t1t.be:8001/apis/ \
   --data 'request_path=/dev/apiengine/v1' \
   --data 'name=dev.apiengine.v1' \
-  --data 'upstream_url=http://api.t1t.be/API-Engine-web/v1/' \
+  --data 'upstream_url=http://dev.api.t1t.be/API-Engine-web/v1/' \
   --data 'strip_request_path=true'
 
 #enable CORS
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
     --data "name=cors" \
     --data "config.origin=*" \
     --data "config.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
@@ -17,28 +17,28 @@ curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
     --data "config.max_age=3600"
 
 #enable Keyauth
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengine.v1/plugins \
     --data "name=key-auth" \
     --data "config.key_names=apikey"
 
 #create marketplace consumer, username should be unique
-curl -X POST http://apim.t1t.be:8001/consumers \
+curl -X POST http://dev.apim.t1t.be:8001/consumers \
     --data "username=dev.marketplace.v1"
 
 #create publisher consumer
-curl -X POST http://apim.t1t.be:8001/consumers \
+curl -X POST http://dev.apim.t1t.be:8001/consumers \
     --data "username=dev.publisher.v1"
 
 #enable keyauth for marketplace and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://apim.t1t.be:8001/consumers/dev.marketplace.v1/key-auth \
+curl -X POST http://dev.apim.t1t.be:8001/consumers/dev.marketplace.v1/key-auth \
     --data "key=6b8406cc81fe4ca3cc9cd4a0abfb97c2"
 
 #enable keyauth for publisher and return API key, result should be captured and is the API key for the given consumer
-curl -X POST http://apim.t1t.be:8001/consumers/dev.publisher.v1/key-auth \
+curl -X POST http://dev.apim.t1t.be:8001/consumers/dev.publisher.v1/key-auth \
     --data "key=***REMOVED***"
 
 #Analytics
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins/ \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengine.v1/plugins/ \
     --data "name=mashape-analytics" \
     --data "config.service_token=558a95f80f7a734609de5c04"
 
@@ -47,14 +47,14 @@ curl -X POST http://apim.t1t.be:8001/apis/dev.apiengine.v1/plugins/ \
 
 #register api engine authorization endpoints
 curl -i -X POST \
-  --url http://apim.t1t.be:8001/apis/ \
+  --url http://dev.apim.t1t.be:8001/apis/ \
   --data 'request_path=/dev/apiengineauth/v1' \
   --data 'name=dev.apiengineauth.v1' \
-  --data 'upstream_url=http://api.t1t.be/API-Engine-auth/v1/' \
+  --data 'upstream_url=http://dev.api.t1t.be/API-Engine-auth/v1/' \
   --data 'strip_request_path=true'
 
 #enable CORS
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
     --data "name=cors" \
     --data "config.origin=*" \
     --data "config.methods=GET,HEAD,PUT,PATCH,POST,DELETE" \
@@ -63,11 +63,11 @@ curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
     --data "config.max_age=3600"
 
 #enable Keyauth
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins \
     --data "name=key-auth" \
     --data "config.key_names=apikey"
 
 #Analytics
-curl -X POST http://apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins/ \
+curl -X POST http://dev.apim.t1t.be:8001/apis/dev.apiengineauth.v1/plugins/ \
     --data "name=mashape-analytics" \
     --data "config.service_token=558a95f80f7a734609de5c04"
