ea9720b92e071ff63e7f16e4110a28658a8c086c
Michallis Pashidis <michallis@trust1tream.com>
2015-11-26 20:30:08 +0100

    actionfacade test
    Former-commit-id: 9822a56989b0577e24255cbdcf1dfa50511c151f

--- API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/facades/ActionFacadeTest.java
+++ API-Engine-ejb/src/test/java/com/t1t/digipolis/apim/facades/ActionFacadeTest.java
@@ -1,32 +1,57 @@
 package com.t1t.digipolis.apim.facades;
 
-import org.junit.After;
-import org.junit.Before;
+import com.t1t.digipolis.apim.beans.actions.ActionBean;
+import com.t1t.digipolis.apim.beans.actions.ActionType;
+import com.t1t.digipolis.apim.core.*;
+import com.t1t.digipolis.apim.gateway.IGatewayLinkFactory;
+import com.t1t.digipolis.apim.security.ISecurityContext;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by michallispashidis on 3/09/15.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ActionFacade.class)
 public class ActionFacadeTest {
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
+    private static final Logger _LOG = LoggerFactory.getLogger(OrganizationFacadeTest.class.getName());
+    @Rule public ExpectedException thrown = ExpectedException.none();
+    @Mock EntityManager em;
+    @Mock ISecurityContext securityContext;
+    @Mock IStorage storage;
+    @Mock IStorageQuery query;
+    @Mock IIdmStorage idmStorage;
+    @Mock IApiKeyGenerator apiKeyGenerator;
+    @Mock IApplicationValidator applicationValidator;
+    @Mock IServiceValidator serviceValidator;
+    @Mock IMetricsAccessor metrics;
+    @Mock GatewayFacade gatewayFacade;
+    @Mock IGatewayLinkFactory gatewayLinkFactory;
+    @Mock UserFacade userFacade;
+    @Mock RoleFacade roleFacade;
+    @InjectMocks ActionFacade actionFacade;
+    @Mock OrganizationFacade orgFacade;
 
     @Test
-    public void testPerformAction() throws Exception {
+    public void testPerformActionLockPlan() throws Exception {
+        ActionBean actionBean = new ActionBean();
+        actionBean.setEntityId("entityId");
+        actionBean.setEntityVersion("version");
+        actionBean.setOrganizationId("orgid");
+        actionBean.setType(ActionType.lockPlan);
 
     }
 
-    public void testPerformAction1() throws Exception {
-
-    }
 }
