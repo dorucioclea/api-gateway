48640f0c0cd9e383a0902b9be9277e11ccf04bca
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-05-04 09:59:07 +0200

    fixed contract issue
    Former-commit-id: 8dab80b9fb3cf35601e199a36a4c640fe1b585ae

--- API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/facades/OrganizationFacade.java
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/facades/OrganizationFacade.java
@@ -443,6 +443,9 @@ public class OrganizationFacade {//extends AbstractFacade<OrganizationBean>
         try {
             //verify if serviceversion and appversion exist
             ApplicationVersionBean avb = getAppVersion(bean.getApplicationOrg(), bean.getApplicationId(), bean.getApplicationVersion());
+            if (avb.getStatus() == ApplicationStatus.Registered) {
+                throw ExceptionFactory.invalidApplicationStatusException();
+            }
             ServiceVersionBean svb = getServiceVersion(organizationId, serviceId, version);
             String appId = ConsumerConventionUtil.createAppUniqueId(avb);
             String svcId = ServiceConventionUtil.generateServiceUniqueName(svb);
@@ -2657,7 +2660,7 @@ public class OrganizationFacade {//extends AbstractFacade<OrganizationBean>
         if (avb == null) {
             throw ExceptionFactory.applicationNotFoundException(applicationId);
         }
-        if (avb.getStatus() == ApplicationStatus.Registered || avb.getStatus() == ApplicationStatus.Retired) {
+        if (avb.getStatus() == ApplicationStatus.Retired) {
             throw ExceptionFactory.invalidApplicationStatusException();
         }
         ServiceVersionBean svb = storage.getServiceVersion(bean.getServiceOrgId(), bean.getServiceId(), bean.getServiceVersion());
