5b5cf31f46a82ef7543c04f8771d80c5c3b8b98d
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-06-06 12:57:37 +0200

    Don't throw new exception.
    Former-commit-id: 4a25c8b60db15bab5e42ba7ed32eb9312cdbca59

--- API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/facades/OrganizationFacade.java
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/facades/OrganizationFacade.java
@@ -1023,6 +1023,8 @@ public class OrganizationFacade {//extends AbstractFacade<OrganizationBean>
                     svb.setAutoAcceptContracts(bean.getAutoAcceptContracts());
                 }
                 if (AuditUtils.valueChanged(svb.getPlans(), bean.getPlans())) {
+                    String extPlans = AuditUtils.asString_ServicePlanBeans(svb.getPlans());
+                    String newPlans = AuditUtils.asString_ServicePlanBeans(bean.getPlans());
                     data.addChange("plans", AuditUtils.asString_ServicePlanBeans(svb.getPlans()), AuditUtils.asString_ServicePlanBeans(bean.getPlans())); //$NON-NLS-1$
                     if (svb.getPlans() == null) {
                         svb.setPlans(new HashSet<ServicePlanBean>());
@@ -1240,7 +1242,7 @@ public class OrganizationFacade {//extends AbstractFacade<OrganizationBean>
             } catch (Exception e) {
                 // TODO it's ok if the clone fails - we did our best
                 // TODO We could try a little harder
-                throw new SystemErrorException(e);
+                //throw new SystemErrorException(e);
                 /*if (e != null) {
                     Throwable t = e;
                     e = (Exception) t;
