51a460b125defc56ae7f8fff7f74d161f2791e72
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-08-30 11:28:59 +0200

    scopes are now always lowercase

--- API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/gateway/rest/GatewayValidation.java
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/gateway/rest/GatewayValidation.java
@@ -148,7 +148,7 @@ public class GatewayValidation {
         for (KongPluginOAuthScope scope : scopes) {
             if (!StringUtils.isEmpty(scope.getScope())) {
                 //add prefix
-                if(!StringUtils.isEmpty(optionalPrefixId) && !scope.getScope().startsWith(optionalPrefixId)) scope.setScope(optionalPrefixId+OAUTH_SCOPE_CONCAT+scope.getScope());
+                if(!StringUtils.isEmpty(optionalPrefixId) && !scope.getScope().startsWith(optionalPrefixId)) scope.setScope(optionalPrefixId+OAUTH_SCOPE_CONCAT+scope.getScope().toLowerCase());
                 if (StringUtils.isEmpty(scope.getScopeDesc())) scope.setScopeDesc(scope.getScope());
                 responseScopes.add(scope);
             }
