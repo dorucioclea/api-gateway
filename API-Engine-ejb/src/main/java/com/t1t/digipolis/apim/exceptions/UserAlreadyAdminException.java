018d70fabab093093aebda206540f86fc4f35763
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-08-31 16:58:09 +0200

    Various bug fixes

---
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/exceptions/UserAlreadyAdminException.java
@@ -0,0 +1,14 @@
+package com.t1t.digipolis.apim.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Guillaume Vandecasteele
+ * @since 2016
+ */
+public class UserAlreadyAdminException {
+
+    private static final Logger _LOG = LoggerFactory.getLogger(UserAlreadyAdminException.class);
+
+}
