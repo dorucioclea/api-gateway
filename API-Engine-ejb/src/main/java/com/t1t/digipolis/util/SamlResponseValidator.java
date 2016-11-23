bf8c7b07b8ddcc8f633daf2a21a9f30699cf1dee
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-11-23 11:42:16 +0100

    Validation in separate util class

--- API-Engine-ejb/src/main/java/com/t1t/digipolis/util/SamlResponseValidator.java
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/util/SamlResponseValidator.java
@@ -1,51 +1,106 @@
 package com.t1t.digipolis.util;
 
+import com.t1t.digipolis.apim.exceptions.ExceptionFactory;
+import com.t1t.digipolis.apim.exceptions.SAMLAuthException;
+import com.t1t.digipolis.apim.exceptions.i18n.Messages;
+import org.apache.commons.lang3.StringUtils;
+import org.opensaml.saml2.core.Assertion;
 import org.opensaml.saml2.core.Response;
+import org.opensaml.xml.security.credential.Credential;
 import org.opensaml.xml.security.x509.BasicX509Credential;
+import org.opensaml.xml.signature.SignableXMLObject;
+import org.opensaml.xml.signature.Signature;
 import org.opensaml.xml.signature.SignatureValidator;
+import org.opensaml.xml.util.Base64;
 import org.opensaml.xml.validation.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+
+import static com.sun.tools.javac.jvm.ByteCodes.ret;
 
 /**
  * Created by michallispashidis on 15/12/15.
  */
 public class SamlResponseValidator {
+
     private static Logger _LOG = LoggerFactory.getLogger(SamlResponseValidator.class.getName());
-    public static void validateSAMLResponse(String certPath, Response response) throws ValidationException {
-        try {
-            //Get Public Key
-            BasicX509Credential publicCredential = new BasicX509Credential();
-            File publicKeyFile = new File(certPath);
 
-            if (publicKeyFile.exists()) {
-                CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-                InputStream fileStream = new FileInputStream(publicKeyFile);
-                X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(fileStream);
-                fileStream.close();
+    public static void validateSAMLResponse(Response response, String idpEntityId) throws SAMLAuthException {
 
-                X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(certificate.getPublicKey().getEncoded());
-                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-                PublicKey key = keyFactory.generatePublic(publicKeySpec);
-
-                //Validate Public Key against Signature
-                if (key != null) {
-                    publicCredential.setPublicKey(key);
-                    SignatureValidator signatureValidator = new SignatureValidator(publicCredential);
-                    signatureValidator.validate(response.getSignature());
-                }
-            }
-        } catch (Exception e) {
-            throw new ValidationException(e);
+        if (response.getIssuer() != null && !response.getIssuer().getValue().equals(idpEntityId)) {
+            throw ExceptionFactory.samlAuthException(Messages.i18n.format("samlIdpEntity", response.getIssuer().getValue(), idpEntityId));
         }
+
+        validateSignature(response.getSignature(), getCertificate(response));
+
+        response.getAssertions().forEach(assertion -> validateSAMLAssertion(assertion, idpEntityId));
+    }
+
+    private static void validateSignature(Signature signature, X509Certificate certificate) {
+        try {
+
+            BasicX509Credential publicCredential = new BasicX509Credential();
+
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(certificate.getPublicKey().getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey key = keyFactory.generatePublic(publicKeySpec);
+
+            publicCredential.setPublicKey(key);
+            publicCredential.setEntityCertificate(certificate);
+            publicCredential.getEntityCertificateChain().add(certificate);
+
+            SignatureValidator sigValidator = new SignatureValidator(publicCredential);
+
+            sigValidator.validate(signature);
+
+        }
+        catch (ValidationException | NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw ExceptionFactory.samlAuthException(ex.getMessage());
+        }
+    }
+
+    private static X509Certificate getCertificate(SignableXMLObject signableObject) {
+        try {
+            String base64Cert = signableObject.getSignature().getKeyInfo().getX509Datas().get(0).getX509Certificates().get(0).getValue();
+            byte[] decodedCert = Base64.decode(base64Cert);
+            return (X509Certificate) CertificateFactory
+                    .getInstance("X.509")
+                    .generateCertificate(new ByteArrayInputStream(decodedCert));
+        }
+        catch (Exception ex) {
+            throw ExceptionFactory.samlAuthException("samlSignatureCertificate");
+        }
+    }
+
+
+    private static void validateSAMLAssertion(Assertion assertion, String idpEntityId) throws SAMLAuthException {
+        //Validate IDP EntityID
+        if (assertion.getIssuer() != null && !assertion.getIssuer().getValue().equals(idpEntityId)) {
+            throw ExceptionFactory.samlAuthException(Messages.i18n.format("samlIdpEntity", assertion.getIssuer().getValue(), idpEntityId));
+        }
+        //Validate assertion conditions
+        if (assertion.getConditions().getNotBefore() != null && assertion.getConditions().getNotBefore().isAfterNow()) {
+            throw ExceptionFactory.samlAuthException(Messages.i18n.format("samlNotBefore"));
+        }
+        if (assertion.getConditions().getNotOnOrAfter() != null
+                && (assertion.getConditions().getNotOnOrAfter().isBeforeNow() || assertion.getConditions().getNotOnOrAfter().isEqualNow())) {
+            throw ExceptionFactory.samlAuthException(Messages.i18n.format("samlNotAfter"));
+        }
+        //Validate assertion signature
+        validateSignature(assertion.getSignature(), getCertificate(assertion));
     }
 }
