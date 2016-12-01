ed8f115a490c907063913338dc3764dfbfdaee9d
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-12-01 16:01:34 +0100

    Maintenance mode working, fix for being unable to disable it

---
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/maintenance/MaintenanceController.java
@@ -0,0 +1,14 @@
+package com.t1t.digipolis.apim.maintenance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Guillaume Vandecasteele
+ * @since 2016
+ */
+public class MaintenanceController {
+
+    private static final Logger _LOG = LoggerFactory.getLogger(MaintenanceController.class);
+
+}
---
+++ API-Engine-web/src/main/java/com/t1t/digipolis/apim/rest/resources/IMaintenanceResource.java
@@ -0,0 +1,14 @@
+package com.t1t.digipolis.apim.rest.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Guillaume Vandecasteele
+ * @since 2016
+ */
+public class IMaintenanceResource {
+
+    private static final Logger _LOG = LoggerFactory.getLogger(IMaintenanceResource.class);
+
+}
