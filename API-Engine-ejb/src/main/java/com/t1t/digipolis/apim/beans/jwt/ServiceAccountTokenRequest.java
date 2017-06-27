4d53924ca9284c6cc6d1b86328b818e548274c56
Guillaume Vandecasteele <guillaume@trust1team.com>
2017-06-27 18:13:19 +0200

    User impersonation for service account token with admin privileges

---
+++ API-Engine-ejb/src/main/java/com/t1t/digipolis/apim/beans/jwt/ServiceAccountTokenRequest.java
@@ -0,0 +1,14 @@
+package com.t1t.digipolis.apim.beans.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Guillaume Vandecasteele
+ * @since 2017
+ */
+public class ServiceAccountTokenRequest {
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceAccountTokenRequest.class);
+
+}
